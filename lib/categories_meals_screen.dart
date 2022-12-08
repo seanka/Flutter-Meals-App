579680ed299be68b6f50e62040921ea35951762a
seanka <and.seanka@gmail.com>
2022-12-08 20:23:57 +0700

    fixed category meals screen to show selected category meals

--- lib/categories_meals_screen.dart
+++ lib/categories_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const String routeName = '/categories-meals';
   const CategoryMealsScreen({super.key});
@@ -10,9 +12,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] as String;
     final categoryId = routeArgs['id'] as String;
+    final categoryMeals = DUMMY_MEALS.where(
+      (meal) {
+        return meal.categories.contains(categoryId);
+      },
+    ).toList();
 
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle)),
-    );
+        appBar: AppBar(title: Text(categoryTitle)),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
