89f8cb44ba0572570c2c0d5cbeecc0abacbafee7
seanka <and.seanka@gmail.com>
2022-12-08 16:47:17 +0700

    added category item styling

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
+import './category_item.dart';
+import './dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20),
-      children: <Widget>[],
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(catData.title, catData.color),
+          )
+          .toList(),
     );
   }
 }
