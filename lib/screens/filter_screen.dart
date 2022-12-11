a95ab1ffa5c4f8bb1d00ed49f324feee8c84ba2a
seanka <and.seanka@gmail.com>
2022-12-11 18:26:54 +0700

    replaced navigator push to navigator replacement

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FilterScreen extends StatelessWidget {
   static const routeName = '/filters';
   const FilterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters!'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(child: Text('Filters')),
+    );
   }
 }
